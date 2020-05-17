a2c461c16b1ac2fb756aa29b7199b4df06d8f5f1
Swati D <60013603+swati-dontamsetti@users.noreply.github.com>
2020-05-16 20:53:52 -0400

    final data added

--- static/js/app.js
+++ static/js/app.js
@@ -1,85 +1,104 @@
-var dropDownMenu = d3.select("#selDataset")
+// fills in dashboard with information and plots
+function optionChanged(metadata, samples) {
+    d3.json("../data/samples.json").then(function(data) {
+        // value of the dropdown menu selected
+        var subject_id = d3.select("#selDataset").node().value
+        
+        // identify metadata and sample data
+        var metadata = data.metadata.filter(function(d) {return d.id == subject_id})[0]
+        var samples = data.samples.filter(function (d) {return d.id == subject_id})
+        
+        // add metadata to Demographics chart
+        var demoData = d3.select("#sample-metadata");
 
+        // clear previous data before entering new data
+        demoData.text("")
 
+        Object.entries(metadata).forEach(([key, value]) => {
+            demoData.append("p").text(`${key}: ${value}`);
+        });
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index]
+        // sort the data from most to least sample values
+        samples.sort(function(a,b){
+            return parseInt(b.sample_values) - parseInt(a.sample_values)
+        })
+
+        // create arrays of the information (both complete data and top data)
+        var otu_ids = samples[0].otu_ids
+        var top_otu_ids = otu_ids.slice(0,10).reverse()
+        var otu_labels = samples[0].otu_labels
+        var top_otu_labels = otu_labels.slice(0,10).reverse()
+        var sample_values = samples[0].sample_values
+        var top_sample_values = sample_values.slice(0,10).reverse()
+        
+        // convert into string with OTU in front to show properly
+        var ticks = top_otu_ids.map(otu => `OTU ${otu}`);
+
+        // formatting the bar chart
+        var trace1 = {
+            x: top_sample_values,
+            y: ticks,
+            text: top_otu_labels,
+            type: "bar",
+            orientation: "h"
+        }
+
+        var barChartData = [trace1]
+
+        var layout = {
+            title: `Top 10 OTUs`,
+            xaxis: {title: "Sample Value"}
+        }
+
+        Plotly.newPlot("bar", barChartData, layout)
+        
+        // create bubble chart of ALL samples
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                color: otu_ids,
+                colorscale: [[0, 'blue'], [1, 'red']],
+                opacity: 0.75,
+                size: sample_values,
+                sizeref: 0.05,
+                sizemode: "area"
+            }
+        }
+        
+        var bubbleChartData = [trace2]
+        
+        var layoutBubble = {
+            title: "Sample Value for each OTU ID",
+            height: 500,
+            width: 900,
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample Value"}
+        }
+        
+        Plotly.newPlot('bubble', bubbleChartData, layoutBubble)
     })
 }
 
-function getData() {
+// opening funtion
+function init() {    
+    var dropDownMenu = d3.select("#selDataset")
+
     d3.json("../data/samples.json").then(function(data) {
-        console.log(data)
+        // filling in the dropdown menu
         var names = data.names
 
-        var samples = data.samples
-
-        var metadata = data.metadata
-
-        init(samples, metadata)
-        buildDropDown(names)
-    })
-}
-
-function buildDropDown(names) {
-    for (var name of names) {
-        dropDownMenu.append("option")
-        .attr("value", name)
-        .text(name)
-    }
-}
-
-// default webpage
-function init(samples, metadata) {
-    // very first entry point
-    var id_940 = samples[0]
-
-    // add metadata to Demographics chart
-    var demoData = d3.select("#sample-metadata").append("tbody")
-    Object.entries(metadata[0]).map(([key, value]) =>{
-        demoData.append("tr").text(`${key}: ${value}`)
-    })
-    
-    // combine the separate arrays into an object of arrays
-    var newArr = []
-    
-    for (i = 0; i < id_940.otu_ids.length; i++) {
-        var entry = {
-            "otu_ids": id_940.otu_ids[i],
-            "otu_labels": id_940.otu_labels[i],
-            "sample_values": id_940.sample_values[i]
+        for (var name of names) {
+            dropDownMenu.append("option")
+            .attr("value", name)
+            .text(name)
         }
-        newArr.push(entry)
-    }
-
-    // find the the top 10 to create a horizontal bar chart
-    newArr.sort(function(a,b){
-        return parseInt(b.sample_values) - parseInt(a.sample_values)
     })
-
-    top10 = newArr.slice(0,10)
-
-    top10 = top10.reverse()
-
-    var trace1 = {
-        x: top10.map(row => row.sample_values),
-        y: top10.map(row => `OTU ${row.otu_ids}`),
-        text: top10.map(row => row.otu_labels),
-        type: "bar",
-        orientation: "h"
-    }
-
-    var barChartData = [trace1]
-
-    var layout = {
-        title: `Top 10 OTUs found in Subject ${metadata[0].id}`
-    }
-
-    Plotly.newPlot("bar", barChartData, layout)
-
-    console.log(top10.map(row => row.otu_ids))
-
+    
+    // calling function to generate plots
+    optionChanged()
 }
 
-getData()
+init()
